b55849d1328bc1b5b711fb79996589df9c6a0464
keidy <ksidy6084@gmail.com>
2021-04-02 18:53:33 +0000

    cart error

--- app/Http/Controllers/cartController.php
+++ app/Http/Controllers/cartController.php
@@ -25,7 +25,7 @@ class cartController extends Controller
 
         $product = product::find($request->livre_id);
         Cart::add($product->id,$product->title, 1, $product->prix)
-        ->associate('App\Models\Product');
+        ->associate('App\Product');
         return redirect()->route('cart.index')->with('success','le produit a ete ajouter avec succes');
 
     }
