ac243d0c00d2eca6d67106b26a0fd5f0cba52350
keidy <ksidy6084@gmail.com>
2021-04-06 23:13:26 +0000

    logo

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -186,7 +186,7 @@ function showSlides() {
                             <div class="col-md-2 isotopeSelector photography">
                         <figure class="col-md-2" >
                         <article>
-                                <img src="{{ asset('livre/images/'.$$product->livre_image_aws_storage_path) }}" alt="" style="">
+                                <img src="{{ asset('livre/images/'.$product->livre_image_aws_storage_path) }}" alt="" style="">
                                 <div class="overlay-background">
                                     <div class="inner"></div>
                                 </div>
