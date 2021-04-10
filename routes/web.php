1ad635adf4704e1be7f401c233835eb60de00b5e
keidy <ksidy6084@gmail.com>
2021-04-10 07:55:19 +0000

    store

--- routes/web.php
+++ routes/web.php
@@ -19,25 +19,26 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', [productController::class, 'index'])->name('index');
+	
+	 						//Product
 
 Route::get('/livreAll',[productController::class , 'indexAll'])->name('livre.indexAll');
-
-Route::get('/cart',[cartController::class, 'index'])->name('cart.index');
 Route::get('/livre/{id}',[productController::class, 'show'])->name('livre.detail');
 Route::get('createlivre',[productController::class, 'create'])->name('livre.create');
 Route::post('storelivre',[productController::class, 'store'])->name('livre.store');
+Route::get('category/{id}',[productController::class, 'category'])->name('livre.category');
+							
+							//Cart
 
 Route::Post('/cart/ajouter',[cartController::class, 'store'])->name('cart.store');
-
+Route::get('/cart',[cartController::class, 'index'])->name('cart.index');
 Route::patch('/panier/${rowId}',[cartController::class, 'update'])->name('cart.update');
-
 Route::delete('/cart/{rowId}',[cartController::class, 'destroy'])->name('cart.destroy');
-
 Route::get('cartp',[cartController::class, 'destroyall'])->name('cart.destroyall');
-Route::get('category/{id}',[productController::class, 'category'])->name('livre.category');
 
 Route::get('retour', [cartController::class, 'retour'])->name('retour');
-//Route::get('category',[productController::class, 'category'])->name('retour');
+
+							//Cheickout
 
 Route::get('/paiement', [checkoutController::class, 'index'])->name('checkout.index');
 Route::Post('/paiement', [checkoutController::class, 'store'])->name('checkout.store');
