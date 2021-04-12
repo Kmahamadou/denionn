413cb5c0677526186ca3681266a00d7db12efda1
keidy <ksidy6084@gmail.com>
2021-04-12 18:39:57 +0000

    slide

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -154,7 +154,7 @@ Image credits: unsplash.com
       <div class="slide-1"></div>
       <div class="hero">
         <hgroup>
-            <h1>We are creative</h1>        
+            <h1>Denionn Expert</h1>        
             <h3>Get start your next awesome project</h3>
         </hgroup>
         <button class="btn btn-hero btn-lg" role="button">See all features</button>
