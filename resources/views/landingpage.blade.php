b688353a2f11a6dcf0b33944f462df827edd6e37
keidy <ksidy6084@gmail.com>
2021-04-18 18:59:00 +0000

    change template

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -154,9 +154,9 @@
         <div class="container">
                 <div class="row justify-content-center mb-3 pb-3">
           <div class="col-md-12 heading-section text-center ftco-animate">
-            <span class="subheading">Featured Products</span>
-            <h2 class="mb-4">Our Products</h2>
-            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
+            <span class="subheading">Articles et produits</span>
+            <h2 class="mb-4">Nos Livres</h2>
+            <p>“Lis avec lenteur à une époque où l'on nous parle de lecture rapide et de lecture en diagonale.”</p>
           </div>
         </div>          
         </div>
