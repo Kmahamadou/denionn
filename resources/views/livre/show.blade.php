6c52bc127c3578c43abf5bf5e2b62a2a4b0fa18b
keidy <ksidy6084@gmail.com>
2021-04-18 19:28:55 +0000

    change template

--- resources/views/livre/show.blade.php
+++ resources/views/livre/show.blade.php
@@ -22,7 +22,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-6 mb-5 ftco-animate">
-                    <a href="https://ikavignetti-assets.s3.us-east-2.amazonaws.com/{{$livre->livre_image_aws_storage_path}}" class="image-popup"><img src="images/product-1.jpg" class="img-fluid" alt="Colorlib Template"></a>
+                    <a href="https://ikavignetti-assets.s3.us-east-2.amazonaws.com/{{$livre->livre_image_aws_storage_path}}" class="image-popup"><img src="images/product-1.jpg" class="img-fluid" alt="Article image"></a>
                 </div>
                 <div class="col-lg-6 product-details pl-md-5 ftco-animate">
                     <h3>{{$livre->title}}</h3>
