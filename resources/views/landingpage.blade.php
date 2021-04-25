09ff025571f8a462e3bcbf25d5e96b5548fe02cd
keidy <ksidy6084@gmail.com>
2021-04-25 01:11:42 +0000

    bouton explore slide

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -288,13 +288,14 @@
           <div class="col-md-12">
             <div class="carousel-testimony owl-carousel">
               <div class="item">
-                <div class="testimony-wrap p-4 pb-5">
-                </div>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/jd8d71oEH5s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
               </div>
               <div class="item">
                 <div class="testimony-wrap p-4 pb-5">
                   <div class="user-img mb-5" style="background-image: url(images/person_2.jpg)">
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/jd8d71oEH5s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <span class="quote d-flex align-items-center justify-content-center">
+                      <i class="icon-quote-left"></i>
+                    </span>
                   </div>
                   <div class="text text-center">
                     <p class="mb-5 pl-4 line">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
