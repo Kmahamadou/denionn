9a7bfaa291bc2a769b0b653b69c04e9688f99426
keidy <ksidy6084@gmail.com>
2021-04-25 00:26:14 +0000

    bouton explore slide

--- resources/views/livre/show.blade.php
+++ resources/views/livre/show.blade.php
@@ -77,9 +77,9 @@
              <form method="post" action="{{route('cart.store')}}">
                         @csrf
                         <input type="hidden"name="livre_id" value="{{$livre->id}}">
-                        <p> <button type="submit">
-                           <a href="#" class="btn btn-black py-3 px-5"> Ajouter au panier</a>
-                         </button></p>
+                        <p><a href="" class="btn btn-black py-3 px-5"> <input type="submit">
+                            Ajouter au panier</a>
+                         </p>
                      </form>
                 </div>
             </div>
