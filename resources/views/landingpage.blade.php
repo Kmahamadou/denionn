4abd482d4e6e9150713e268b617e445295b45774
keidy <ksidy6084@gmail.com>
2021-04-25 01:00:15 +0000

    bouton explore slide

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -260,9 +260,9 @@
                 <div class="row justify-content-end">
           <div class="col-md-6 heading-section ftco-animate deal-of-the-day ftco-animate">
             <span class="subheading">Best Price For You</span>
-            <h2 class="mb-4">Deal of the day</h2>
+            <h2 class="mb-4">Denionn Experts</h2>
             <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
-            <h3><a href="#">Spinach</a></h3>
+{{--             <h3><a href="#">Spinach</a></h3>
             <span class="price">$10 <a href="#">now $5 only</a></span>
             <div id="timer" class="d-flex mt-5">
                           <div class="time" id="days"></div>
@@ -270,7 +270,7 @@
                           <div class="time pl-3" id="minutes"></div>
                           <div class="time pl-3" id="seconds"></div>
                         </div>
-          </div>
+          </div> --}}
         </div>          
         </div>
     </section>
