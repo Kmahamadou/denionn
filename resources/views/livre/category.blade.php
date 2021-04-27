ea8459da1c250e6903c1919db674ac9e0e9d7e84
keidy <ksidy6084@gmail.com>
2021-04-27 00:47:15 +0000

    list product

--- resources/views/livre/category.blade.php
+++ resources/views/livre/category.blade.php
@@ -1,30 +1,80 @@
-@extends('layout.main')
-    @foreach (  $products as $product)
-                           <a href="{{Route('livre.detail', $product->id)}}">
-                            <div class="col-md-6 isotopeSelector photography">
-                        <article class="">
-                            
-                                <img src="https://ikavignetti-assets.s3.us-east-2.amazonaws.com/{{$product->livre_image_aws_storage_path}}" alt="">
-                                <div class="overlay-background">
-                                    <div class="inner"></div>
+@include('layout.header')
+
+    <div class="hero-wrap hero-bread" style="background-image: url('images/bgd1.jpg');">
+      <div class="container">
+        <div class="row no-gutters slider-text align-items-center justify-content-center">
+          <div class="col-md-9 ftco-animate text-center">
+            <p class="breadcrumbs"><span class="mr-2"><a href="/">Home</a></span> <span>Articles</span></p>
+            <h1 class="mb-0 bread">Livres</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <section class="ftco-section">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-10 mb-5 text-center">
+                    <ul class="product-category">
+                        <li><a href="{{route('livre.indexAll')}}" class="active">Tout</a></li>
+                        <li><a href="{{ route('livre.category', "Entreprise") }}">Entréprise</a></li>
+                        <li><a href="{{ route('livre.category', "Marketing internet") }}">Marketing</a></li>
+                        <li><a href="{{ route('livre.category', "Santé et bien être") }}">Santé</a></li>
+                        <li><a href="{{ route('livre.category', "Developpement personnel") }}">Dev-personnel</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row">
+            @foreach (  $products as $product)
+                <div class="col-md-6 col-lg-3 ftco-animate">
+                    <div class="product">
+                        <a href="{{Route('livre.detail', $product->id)}}" class="img-prod"><img class="img-fluid" src="https://ikavignetti-assets.s3.us-east-2.amazonaws.com/{{$product->livre_image_aws_storage_path}}" alt="{{$product->title}}">
+                            <div class="overlay"></div>
+                        </a>
+                        <div class="text py-3 pb-4 px-3 text-center">
+                            <h3><a href="#">{{$product->title}}</a></h3>
+                            <div class="d-flex">
+                                <div class="pricing">
+                                    <p class="price"><span class="price-sale">{{$product->prix}} f cfa</span></p>
                                 </div>
-                                <div class="overlay">
-                                    <div class="inner-overlay">
-                                        <div class="row margin-0 project-content">
-                                            <div class="col-md-12 info-head">
-                                                <h3>{{$product->title}}</h3></h3>
-                                                <h4>{{$product->categorie}}</h4>
-                                            </div>
-                                            <div class="col-md-12 info">
-                                                <p>{{$product->description}}</p>
-                                                <p><a title="Project Image" class="fancybox-pop fancybox.image" href="http://placehold.it/625x450"><i class="fa fa-search fa-border fa-2x"></i></a>
-                                                    <a title="Project Link" href="portfolio-item.html"><i class="fa fa-link fa-border fa-2x"></i></a></p>
-                                            </div>
-                                        </div>
-                                    </div>
+                            </div>
+                            <div class="bottom-area d-flex px-3">
+                                <div class="m-auto d-flex">
+                                    <a href="#" class="add-to-cart d-flex justify-content-center align-items-center text-center">
+                                        <span><i class="ion-ios-menu"></i></span>
+                                    </a>
+                                    <a href="{{Route('livre.detail', $product->id)}}" class="buy-now d-flex justify-content-center align-items-center mx-1">
+                                        <span><i class="ion-ios-cart"></i></span>
+                                    </a>
+                                    <a href="#" class="heart d-flex justify-content-center align-items-center ">
+                                        <span><i class="ion-ios-heart"></i></span>
+                                    </a>
                                 </div>
-                           
-                        </article>
-                    </div></a>
-                        @endforeach</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+            </div>
+            <div class="row mt-5">
+{{--           <div class="col text-center">
+            <div class="block-27">
+              <ul>
+                <li><a href="#">&lt;</a></li>
+                <li class="active"><span>1</span></li>
+                <li><a href="#">2</a></li>
+                <li><a href="#">3</a></li>
+                <li><a href="#">4</a></li>
+                <li><a href="#">5</a></li>
+                <li><a href="#">&gt;</a></li>
+              </ul>
+            </div>
+          </div>
+        </div> --}}
+        </div>
+    </section>
+
+@include('layout/footer')
+
+@include('layout/scriptjs')
  
