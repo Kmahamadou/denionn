2d10388705873d2be32dc2bd4678857fab9f1300
Mahamadou <kmahamadou01@gmail.com>
2021-04-28 14:00:50 +0000

    Arranged footer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -11,7 +11,7 @@
           <div class="col-md">
             <div class="ftco-footer-widget mb-4">
               <h2 class="ftco-heading-2">Denionn Experts</h2>
-              <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia.</p>
+              <p>Lire délivre</p>
               <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
                 <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
                 <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
@@ -19,20 +19,9 @@
               </ul>
             </div>
           </div>
-          <div class="col-md">
-            <div class="ftco-footer-widget mb-4 ml-md-5">
-              <h2 class="ftco-heading-2">Menu</h2>
-              <ul class="list-unstyled">
-                <li><a href="/" class="py-2 d-block">Accueil</a></li>
-                <li><a href="{{route('livre.indexAll')}}" class="py-2 d-block">Boutique</a></li>
-                <li><a href="#" class="py-2 d-block">A-propos</a></li>
-                <li><a href="#" class="py-2 d-block">Contact Us</a></li>
-              </ul>
-            </div>
-          </div>
           <div class="col-md-4">
              <div class="ftco-footer-widget mb-4">
-              <h2 class="ftco-heading-2">Help</h2>
+              <h2 class="ftco-heading-2">Accès rapide</h2>
               <div class="d-flex">
                   <ul class="list-unstyled mr-l-5 pr-l-3 mr-4">
                     <li><a href="{{ route('livre.category', "Entreprise") }}" class="py-2 d-block">Entréprise</a></li>
@@ -52,8 +41,8 @@
                 <h2 class="ftco-heading-2">Qui sommes-nous?</h2>
                 <div class="block-23 mb-3">
                   <ul>
-                    <li><span class="icon icon-map-marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
-                    <li><a href="#"><span class="icon icon-phone"></span><span class="text">+223 00 00 00 00</span></a></li>
+                    <li><span class="icon icon-map-marker"></span><span class="text">Sotuba ACI, près de la Rue des armées</span></li>
+                    <li><a href="#"><span class="icon icon-phone"></span><span class="text">+223 77 09 95 43</span></a></li>
                     <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@Denionn.com</span></a></li>
                   </ul>
                 </div>
@@ -64,7 +53,7 @@
           <div class="col-md-12 text-center">
 
             <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                          Copyright &copy;<script>document.write(new Date().getFullYear());</script> Tout droit reseré | Denionn Experts<i class="icon-heart color-danger" aria-hidden="true"></i><a href="https://colorlib.com" target="_blank"></a>.  <a href="https://themeslab.org/" target="_blank"></a>
+                          Copyright &copy;<script>document.write(new Date().getFullYear());</script> Tous droits reserés | Denionn Experts<i class="icon-heart color-danger" aria-hidden="true"></i><a href="https://colorlib.com" target="_blank"></a>.  <a href="https://themeslab.org/" target="_blank"></a>
                           <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                         </p>
           </div>
--- resources/views/livre/index.blade.php
+++ resources/views/livre/index.blade.php
@@ -35,7 +35,7 @@
                             <h3><a href="#">{{$product->title}}</a></h3>
                             <div class="d-flex">
                                 <div class="pricing">
-                                    <p class="price"><span class="price-sale">{{$product->prix}} f cfa</span></p>
+                                    <p class="price"><span class="price-sale">{{$product->prix}} fcfa</span></p>
                                 </div>
                             </div>
                             <div class="bottom-area d-flex px-3">
