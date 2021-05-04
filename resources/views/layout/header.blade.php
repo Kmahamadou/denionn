158fcadd192973fe16d9a8fb10f7c8b4d34501d4
keidy <ksidy6084@gmail.com>
2021-05-04 23:46:30 +0000

    list product

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -31,7 +31,7 @@
   <body class="goto-here">
         <div class="py-1 bg-primary">
         <div class="container">
-            <div class="row no-gutters d-flex align-items-start align-items-center px-md-0">
+            <div class="row no-gutters d-flex align-items-center align-items-center px-md-0">
                 <div class="col-lg-12 d-block">
                     <div class="row d-flex">
                         <div class="col-md pr-4 d-flex topper align-items-center">
