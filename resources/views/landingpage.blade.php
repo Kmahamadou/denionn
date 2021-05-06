d782310191f6c52d1e5b95407261b4f05cee750d
keidy <ksidy6084@gmail.com>
2021-05-06 14:16:24 +0000

    list product

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -17,7 +17,7 @@
             </div>
           </div>
 
-          <div class="slider-item" style="background-image: url(images/bgd2.jpg);">
+          <div class="slider-item" style="background-image: url(images/bgd2.jpg); scroll-behavior: auto;">
             <div class="overlay"></div>
             <div class="container">
               <div class="row slider-text justify-content-center align-items-center" data-scrollax-parent="true">
