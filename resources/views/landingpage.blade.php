2f39fa619a8cf969a00689863a914d15127dc251
keidy <ksidy6084@gmail.com>
2021-05-06 14:25:27 +0000

    list product

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -1,11 +1,11 @@
 @include('layout.header')
 
- <section id="home-section" class="hero">
+ <section id="home-section" class="hero" style="scroll-behavior: all;">
           <div class="home-slider owl-carousel" >
           <div class="slider-item" style="background-image: url(images/bgd1.jpg);">
             <div class="overlay"></div>
             <div class="container">
-              <div class="row slider-text justify-content-center align-items-center" >
+              <div class="row slider-text justify-content-center align-items-center" data-scrollax-parent="true">
 
                 <div class="col-md-12 ftco-animate text-center">
                   <h1 class="mb-2">Entreprise &amp; Buisiness</h1>
