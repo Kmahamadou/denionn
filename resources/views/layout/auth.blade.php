e3b354dbcac1be4a7ab07678291607ea35ecfeff
keidy <ksidy6084@gmail.com>
2021-05-08 13:16:58 +0000

    show

--- resources/views/layout/auth.blade.php
+++ resources/views/layout/auth.blade.php
@@ -3,7 +3,7 @@
     <li class="nav-item">
         <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
     </li>
-    @if (Route::has('register'))
+    @if (Route::has('S\'inscrire'))
         <li class="nav-item">
             <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
         </li>
@@ -20,7 +20,7 @@
             <a class="dropdown-item" href="{{ route('logout') }}"
                 onclick="event.preventDefault();
                                 document.getElementById('logout-form').submit();">
-                {{ __('Logout') }}
+                {{ __('Deconnexion') }}
             </a>
 
             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
