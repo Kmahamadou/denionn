e88bb854224bb1dd8817a1137f7a437c120b2b58
keidy <ksidy6084@gmail.com>
2021-05-08 13:24:27 +0000

    show

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,5 @@
 @extends('layout.header')
 
-@section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -20,4 +19,7 @@
         </div>
     </div>
 </div>
-@endsection
+   
+@include('layout/footer')
+
+@include('layout/scriptjs')
