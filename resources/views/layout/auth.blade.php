350fb048cf5e5c9d14999b2b6b2967664e2bd0b7
keidy <ksidy6084@gmail.com>
2021-05-09 01:13:41 +0000

    Roland note

--- resources/views/layout/auth.blade.php
+++ resources/views/layout/auth.blade.php
@@ -1,7 +1,7 @@
 <!-- Authentication Links -->
 @guest
     <li class="nav-item">
-        <a class="nav-link" href="{{ route('login') }}">{{ __('Se connexion') }}</a>
+        <a class="nav-link" href="{{ route('login') }}">{{ __('Se connecter') }}</a>
     </li>
     @if (Route::has('register'))
         <li class="nav-item">
