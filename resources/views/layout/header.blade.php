633312ac8366035e8baf2ba176035c04280094e8
keidy <ksidy6084@gmail.com>
2021-07-08 03:36:57 +0000

    'final'

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -9,24 +9,24 @@
     <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Amatic+SC:400,700&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" href="{{ asset('css/open-iconic-bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/animate.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/open-iconic-bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/animate.css') }}">
     
-    <link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/owl.theme.default.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/magnific-popup.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/owl.theme.default.min.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/magnific-popup.css') }}">
 
-    <link rel="stylesheet" href="{{ asset('css/aos.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/aos.css') }}">
 
-    <link rel="stylesheet" href="{{ asset('css/ionicons.min.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/ionicons.min.css') }}">
 
-    <link rel="stylesheet" href="{{ asset('css/bootstrap-datepicker.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/jquery.timepicker.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/bootstrap-datepicker.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/jquery.timepicker.css') }}">
     <script src="{{ asset('js/app.js') }}" defer></script>
     
-    <link rel="stylesheet" href="{{ asset('css/flaticon.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/icomoon.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/flaticon.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/icomoon.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/style.css') }}">
   </head>
   <body class="goto-here">
         <div class="py-1 bg-primary">
@@ -52,7 +52,7 @@
     </div>
     <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
         <div class="container">
-          <a class="navbar-brand" href="/" style="text-transform: none;"><img style="width: 20%; " src="{{ asset('images/denionnlogo2.png') }}"> Denionn</a>
+          <a class="navbar-brand" href="/" style="text-transform: none;"><img style="width: 20%; " src="{{ secure_asset('images/denionnlogo2.png') }}"> Denionn</a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="oi oi-menu"></span> Menu
           </button>
