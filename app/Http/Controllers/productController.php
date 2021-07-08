fbf66aa9aeb112a99dc5d48a26783c13f5bef422
keidy <ksidy6084@gmail.com>
2021-07-08 03:58:00 +0000

    'final'

--- app/Http/Controllers/productController.php
+++ app/Http/Controllers/productController.php
@@ -28,7 +28,7 @@ class productController extends Controller
 
     public function store(Request $request)
     {   
-        //dd($request);
+        dd($request);
        
          $validatedData = request()->validate([
 
