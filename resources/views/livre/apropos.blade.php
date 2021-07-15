7344d4dc5496f81e39fdfaa768e4beb3605ff8cd
keidy <ksidy6084@gmail.com>
2021-07-15 03:33:04 +0000

    sommaire

--- resources/views/livre/apropos.blade.php
+++ resources/views/livre/apropos.blade.php
@@ -7,7 +7,7 @@
 
         <div class="row block-9">
 
-          <div class="col-md-5 order-md-last mt-2">
+          <div class="col-md-5 order-md-last">
             
           <p> <h3>A propos de nous</h3>Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès qu'il est prêt ou que la mise en page est achevée. Généralement, on utilise un texte en faux latin, le Lorem ipsum ou Lipsum
           Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès qu'il est prêt ou que la mise en page est achevée. Généralement, on utilise un texte en faux latin, le Lorem ipsum ou Lipsum
@@ -15,7 +15,7 @@
           
           </div>
 
-          <div class="col-md-6 d-flex mr-4" style="background-image: url('images/DENIONN.jpg');">
+          <div class="col-md-6 d-flex mr-4 mt-2" style="background-image: url('images/DENIONN.jpg');">
             <div class="bg-white">
               <img src="" alt="">
             </div>
