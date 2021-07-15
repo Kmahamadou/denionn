1e87bead2d50a964f7c9c740f2753f5f7876b5b0
keidy <ksidy6084@gmail.com>
2021-07-15 03:00:56 +0000

    sommaire

--- resources/views/livre/contact.blade.php
+++ resources/views/livre/contact.blade.php
@@ -5,8 +5,8 @@
       <div class="container">
         <div class="row no-gutters slider-text align-items-center justify-content-center">
           <div class="col-md-9 ftco-animate text-center">
-          	<p class="breadcrumbs"><span class="mr-2"><a href="index.html">Accueil</a></span> <span>Contacter nous</span></p>
-            <h1 class="mb-0 bread">Contacter nous</h1>
+          	<p class="breadcrumbs"><span class="mr-2"><a href="index.html">Accueil</a></span> <span></span></p>
+            <h1 class="mb-0 bread"></h1>
           </div>
         </div>
       </div>
