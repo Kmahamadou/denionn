59f05cb3e30b799f2c420cfe6c9018085c15ba20
keidy <ksidy6084@gmail.com>
2021-07-15 03:27:56 +0000

    sommaire

--- resources/views/livre/apropos.blade.php
+++ resources/views/livre/apropos.blade.php
@@ -1,23 +1,13 @@
 
 @include('layout.header')
 
-<div class="hero-wrap hero-bread" >
-      <div class="container">
-        <div class="row no-gutters slider-text align-items-center justify-content-center">
-          <div class="col-md-9 ftco-animate text-center">
-            <p class="breadcrumbs"><span class="mr-2"><a href="#"></a></span> <span></span></p>
-            <h1 class="mb-0 bread"></h1>
-          </div>
-        </div>
-      </div>
-    </div>
 
     <section class="ftco-section contact-section bg-light">
       <div class="container">
 
         <div class="row block-9">
 
-          <div class="col-md-6 order-md-last">
+          <div class="col-md-6 order-md-last mr-4">
             
           <p> <h3>A propos de nous</h3>Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès qu'il est prêt ou que la mise en page est achevée. Généralement, on utilise un texte en faux latin, le Lorem ipsum ou Lipsum
           Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès qu'il est prêt ou que la mise en page est achevée. Généralement, on utilise un texte en faux latin, le Lorem ipsum ou Lipsum
