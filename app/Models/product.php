03b6336d701bc474a3643d9060f325a860308dda
keidy <ksidy6084@gmail.com>
2021-07-15 01:34:07 +0000

    sommaire

--- app/Models/product.php
+++ app/Models/product.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class product extends Model
 {
     use HasFactory;
-    protected $fillable = ['title','auteur','mode','livre','prix','quantite','categorie','description','image'];
+    protected $fillable = ['title','auteur','mode','livre','prix','quantite','categorie','description','image','livre_content_aws_storage_path','livre_image_aws_storage_path','sommaire'];
 }
