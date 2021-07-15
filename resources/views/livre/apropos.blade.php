8117de5f386676cd6b4763606ab3c8f61adbe52f
keidy <ksidy6084@gmail.com>
2021-07-15 03:21:26 +0000

    sommaire

--- resources/views/livre/apropos.blade.php
+++ resources/views/livre/apropos.blade.php
@@ -18,8 +18,8 @@
         <div class="row block-9">
 
           <div class="col-md-6 order-md-last d-flex">
-             <h3>A propos</h3>
-          Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès qu'il est prêt ou que la mise en page est achevée. Généralement, on utilise un texte en faux latin, le Lorem ipsum ou Lipsum
+            {{--  <h3>A propos</h3> --}}
+          <h3>Le lorem ipsum est,</h3> en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès qu'il est prêt ou que la mise en page est achevée. Généralement, on utilise un texte en faux latin, le Lorem ipsum ou Lipsum
           Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès qu'il est prêt ou que la mise en page est achevée. Généralement, on utilise un texte en faux latin, le Lorem ipsum ou Lipsum
           Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès qu'il est prêt ou que la mise en page est achevée. Généralement, on utilise un texte en faux latin, le Lorem ipsum ou Lipsum
           
