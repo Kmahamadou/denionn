9458c3e7a240446a3c9c004759e76b2ede1e3b5c
keidy <ksidy6084@gmail.com>
2021-07-15 03:10:44 +0000

    sommaire

--- resources/views/livre/apropos.blade.php
+++ resources/views/livre/apropos.blade.php
@@ -34,30 +34,16 @@
 
         </div>
         <div class="row block-9">
-          <div class="col-md-8 align-items-center" style="margin-left: 18%;">
-            <form action="#" class="bg-white p-5 contact-form">
-              <div class="form-group">
-                <input type="text" class="form-control" placeholder="Nom">
-              </div>
-              <div class="form-group">
-                <input type="text" class="form-control" placeholder="Email">
-              </div>
-              <div class="form-group">
-                <input type="text" class="form-control" placeholder="Message">
-              </div>
-              <div class="form-group">
-                <textarea name="" id="" cols="30" rows="7" class="form-control" placeholder="Message"></textarea>
-              </div>
-              <div class="form-group">
-                <input type="submit" value="Envoyer" class="btn btn-primary py-3 px-5">
-              </div>
-            </form>
+          <div class="col-md-8 order-md-last d-flex">
+          
           
           </div>
 
-         {{--  <div class="col-md-6 d-flex">
-            <div id="map" class="bg-white"></div>
-          </div> --}}
+          <div class="col-md-6 d-flex">
+            <div class="bg-white">
+              <img src="" alt="">
+            </div>
+          </div> 
         </div>
       </div>
     </section>
