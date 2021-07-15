a7e180790afcf6d68c50c2e8ca18305106df16e7
keidy <ksidy6084@gmail.com>
2021-07-15 02:46:22 +0000

    sommaire

--- resources/views/livre/contact.blade.php
+++ resources/views/livre/contact.blade.php
@@ -1,8 +1,8 @@
 
 @include('layout.header')
 
-<div class="hero-wrap hero-bread" style="background-image: url('images/contactimg1.jpg');">
-      <div class="container">
+<div class="hero-wrap hero-bread" >
+      <div class="container" style="background-image: url('images/contactimg1.jpg');">
         <div class="row no-gutters slider-text align-items-center justify-content-center">
           <div class="col-md-9 ftco-animate text-center">
           	<p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home</a></span> <span>Contact us</span></p>
