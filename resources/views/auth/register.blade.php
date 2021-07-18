8cb395260d2bd9f6e4107a7e467f0311464e7408
unknown <ksidy6084@gmail.com>
2021-07-18 22:25:56 +0000

    Code review

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,5 @@
 @include('layout.header')
-<div class="container mb-5">
+<div class="container mt-5 mb-5">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
