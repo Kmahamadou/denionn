d89d055352880ee41d73f27e6031e1fb03fe7da9
keidy <ksidy6084@gmail.com>
2021-07-19 16:25:11 +0000

    summary

--- resources/views/livre/show.blade.php
+++ resources/views/livre/show.blade.php
@@ -24,7 +24,7 @@
           <div class="col-md-12 heading-section ftco-animate">
             <h3 class="mb-4">De quoi est-il sujet dans ce livre?</h3>
                 @foreach($sommaires as $sommaire)
-                    <p class="align-items-start mb-0 mt-0">{{ $sommaire }}</p>
+                    <h4 class="align-items-start mb-0 mt-0">{{ $sommaire }}</h4>
                 @endforeach
           </div>
         </div>          
