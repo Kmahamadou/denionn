b8111dd9d5323092ed7e43b6ce62e101a1784a79
unknown <ksidy6084@gmail.com>
2021-07-27 12:21:25 +0000

    Latly fixes

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,5 +1,5 @@
        <footer class="ftco-footer ftco-section mt-5">
-      <div class="container">
+      <div class="container justify-content-center">
         <div class="row">
             <div class="mouse">
                         <a href="#" class="mouse-icon">
@@ -8,50 +8,53 @@
                     </div>
         </div>
         <div class="row mb-5">
-          <div class="col-md-4">
-            <div class="ftco-footer-widget mb-4">
-              <h2 class="ftco-heading-2">Denionn</h2>
-              <p>Lire délivre</p>
-              <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
-                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
-                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
-                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
-              </ul>
+          <div class="col-12">
+            <div class="col-md-4">
+              <div class="ftco-footer-widget mb-4">
+                <h2 class="ftco-heading-2">Denionn</h2>
+                <p>Lire délivre</p>
+                <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
+                  <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+                  <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+                  <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+                </ul>
+              </div>
             </div>
-          </div>
-          <div class="col-md-4">
-             <div class="ftco-footer-widget mb-4">
-              <h2 class="ftco-heading-2">Accès rapide</h2>
-              <div class="d-flex">
-                  <ul class="list-unstyled mr-l-5 pr-l-3 mr-4">
-                    <li><a href="{{ route('livre.category', 'Entreprise') }}" class="py-2 d-block">Entreprise</a></li>
-                    <li><a href="{{ route('livre.category', 'Santé et bien être') }}" class="py-2 d-block">Santé &amp; Bien être</a></li>
-                    <li><a href="{{ route('livre.category', 'Marketing internet') }}" class="py-2 d-block">Marketing Internet</a></li>
-                    <li><a href="{{ route('livre.category', 'Developpement personnel') }}" class="py-2 d-block">Développement personnel</a></li>
-                  </ul>
-                </div>
+            <div class="col-md-4">
+               <div class="ftco-footer-widget mb-4">
+                <h2 class="ftco-heading-2">Accès rapide</h2>
+                <div class="d-flex">
+                    <ul class="list-unstyled mr-l-5 pr-l-3 mr-4">
+                      <li><a href="{{ route('livre.category', 'Entreprise') }}" class="py-2 d-block">Entreprise</a></li>
+                      <li><a href="{{ route('livre.category', 'Santé et bien être') }}" class="py-2 d-block">Santé &amp; Bien être</a></li>
+                      <li><a href="{{ route('livre.category', 'Marketing internet') }}" class="py-2 d-block">Marketing Internet</a></li>
+                      <li><a href="{{ route('livre.category', 'Developpement personnel') }}" class="py-2 d-block">Développement personnel</a></li>
+                    </ul>
+                  </div>
+              </div>
             </div>
-          </div>
-          <div class="col-md-4">
-            <div class="ftco-footer-widget mb-4">
-                <h2 class="ftco-heading-2">Qui sommes-nous?</h2>
-                <div class="block-23 mb-3">
-                  <ul>
-                    <li><span class="icon icon-map-marker"></span><span class="text">Sotuba ACI, près de la Rue des armées</span></li>
-                    <li><a href="#"><span class="icon icon-phone"></span><span class="text">+223 77 09 95 43</span></a></li>
-                    <li><a href="mailto:info@denionn.com"><span class="icon icon-envelope"></span><span class="text">info@denionn.com</span></a></li>
-                  </ul>
-                </div>
+            <div class="col-md-4">
+              <div class="ftco-footer-widget mb-4">
+                  <h2 class="ftco-heading-2">Qui sommes-nous?</h2>
+                  <div class="block-23 mb-3">
+                    <ul>
+                      <li><span class="icon icon-map-marker"></span><span class="text">Sotuba ACI, près de la Rue des armées</span></li>
+                      <li><a href="#"><span class="icon icon-phone"></span><span class="text">+223 77 09 95 43</span></a></li>
+                      <li><a href="mailto:info@denionn.com"><span class="icon icon-envelope"></span><span class="text">info@denionn.com</span></a></li>
+                    </ul>
+                  </div>
+              </div>
             </div>
           </div>
         </div>
-        <div class="row">
-          <div class="col-md-12 text-center">
+          <div class="row">
+            <div class="col-md-12 text-center">
 
-            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                          Copyright &copy;<script>document.write(new Date().getFullYear());</script> Tous droits reservés | Denionn <i class="icon-heart color-danger" aria-hidden="true"></i><a href="https://colorlib.com" target="_blank"></a>.  <a href="https://themeslab.org/" target="_blank"></a>
-                          <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                        </p>
+              <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                            Copyright &copy;<script>document.write(new Date().getFullYear());</script> Tous droits reservés | Denionn <i class="icon-heart color-danger" aria-hidden="true"></i><a href="https://colorlib.com" target="_blank"></a>.  <a href="https://themeslab.org/" target="_blank"></a>
+                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                          </p>
+            </div>
           </div>
         </div>
       </div>
