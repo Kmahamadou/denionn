4a0442453f3849b7d23eae4aef80e8e77aa3e2c4
unknown <ksidy6084@gmail.com>
2021-07-30 13:25:07 +0000

    Lately change!

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Str;
+use App\Models\User;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        DB::table('users')->insert([
+            'name' => 'Site Admin',
+            'email' => 'DenionnExperts@denionn.com',
+            'password' => Hash::make('#admin@denionn@experts##'),
+        ]);
+
+
+
+    }
+}
